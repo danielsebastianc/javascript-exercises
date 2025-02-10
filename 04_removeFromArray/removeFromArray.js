0ad136a68b48673061c502f122a6115f5a42506f
DanS <117628985+danielsebastianc@users.noreply.github.com>
2025-02-10 22:53:40 +0700

    add submission for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    let result = array
+    for (let i = 0; i < num.length; i++) {
+        const number = num[i];
+        result = result.filter(element => {
+            return number !== element
+        })
+    }
+    return result
 };
 
 // Do not edit below this line
