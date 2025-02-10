a9954332644d073f145db5e54d55a015cbee6dbf
DanS <117628985+danielsebastianc@users.noreply.github.com>
2025-02-10 23:14:16 +0700

    add submission for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let firstNum = start
+    let secondNum = end
+    if(start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR"
+    }
+    if(start > end){
+        firstNum = end
+        secondNum = start
+    }
+    let sum = 0
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
