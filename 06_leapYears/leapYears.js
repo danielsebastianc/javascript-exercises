7fbee04d843c9bb1e323f0f8da21208371f1d63c
DanS <117628985+danielsebastianc@users.noreply.github.com>
2025-02-21 23:45:56 +0700

    add submission for exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if(years % 100 == 0){
+        if(years % 400 == 0){
+            return true
+        }else{
+            return false
+        }
+    }else if(years % 4 == 0){
+        return true
+    }else{
+        return false
+    }
 };
 
 // Do not edit below this line
