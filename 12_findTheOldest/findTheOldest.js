0c4e6696fd788aef469915ce548563be20f5a20a
DanS <117628985+danielsebastianc@users.noreply.github.com>
2025-02-23 15:42:37 +0700

    add submission for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
+    const peopleWithAge = people
+        .map((person) => {
+            if (!person.yearOfDeath) {
+                person["age"] = currentYear - person.yearOfBirth;
+            } else {
+                person["age"] = person.yearOfDeath - person.yearOfBirth;
+            }
+            return person;
+        })
+        .reduce((oldestPerson, currentPerson) => {
+            return oldestPerson.age < currentPerson.age
+                ? currentPerson
+                : oldestPerson;
+        });
+    return peopleWithAge;
 };
 
 // Do not edit below this line
