2874f8eb95d4fdf0943f487ab8b2def38e0a8a4f
DanS <117628985+danielsebastianc@users.noreply.github.com>
2025-02-23 13:47:36 +0700

    add submission for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let fibCount;
+    if (typeof num !== "number") {
+        fibCount = parseInt(num);
+    } else {
+        fibCount = num;
+    }
 
+    if (fibCount < 0) {
+        return "OOPS";
+    }
+    if (fibCount == 0) {
+        return 0;
+    }
+
+    let firstNum = 1;
+    let secondNum = 0;
+
+    for (let i = 2; i <= fibCount; i++) {
+        let temp = firstNum + secondNum;
+        secondNum = firstNum;
+        firstNum = temp;
+    }
+    return firstNum;
 };
 
 // Do not edit below this line
