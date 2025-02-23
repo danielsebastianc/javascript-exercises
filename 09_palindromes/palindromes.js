70dd2fe9c73b2917a0ba51d18970c23cd6d61196
DanS <117628985+danielsebastianc@users.noreply.github.com>
2025-02-23 13:27:53 +0700

    add submission for exercise 08

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanum.includes(char))
+    .join("");
+  const reverseString = cleanString.split("").reverse().join("");
+  return reverseString == cleanString;
 };
 
 // Do not edit below this line
